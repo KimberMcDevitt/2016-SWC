81db82a9b3dc42ede628e4ab5dca27237db507a6
KimberMcDevitt <ki.mcdevitt@northeastern.edu>
2016-10-08 16:55:21 -0400

    Update 330Lines.js

--- ps-04/330Lines.js
+++ ps-04/330Lines.js
@@ -1,10 +1,10 @@
 var myCanvas = document.getElementById('canvasElm');
 var drawingPad = myCanvas.getContext('2d');
 
-var numLines = 33;
+var numLines = 330;
 var cx = document.querySelector('canvas').getContext('2d');
 cx.beginPath();
-for (var y = 10; y<100*(numLines+2); y+=10){
+for (var y = 10; y<=numLines*5; y+=5){
   cx.moveTo(10+y,y);
   cx.lineTo(200,y);
 }

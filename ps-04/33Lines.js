bb4f9c6735d8bf40a34482c3c56694838a332003
KimberMcDevitt <ki.mcdevitt@northeastern.edu>
2016-10-08 16:25:46 -0400

    Create 33Lines.js

---
+++ ps-04/33Lines.js
@@ -0,0 +1,11 @@
+var myCanvas = document.getElementById('canvasElm');
+var drawingPad = myCanvas.getContext('2d');
+
+var numLines = 33;
+var cx = document.querySelector('canvas').getContext('2d');
+cx.beginPath();
+for (var y = 10; y<100*numLines; y+=10){
+  cx.moveTo(10+y,y);
+  cx.lineTo(200,y);
+}
+cx.stroke();

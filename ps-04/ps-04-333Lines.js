f2cb3084f061f11256e02f74090baebc2d3441dd
KimberMcDevitt <ki.mcdevitt@northeastern.edu>
2016-10-08 22:16:18 -0400

    Update ps-04-333Lines.js

--- ps-04/ps-04-333Lines.js
+++ ps-04/ps-04-333Lines.js
@@ -1,11 +1,15 @@
 var c = document.getElementById("canvasElm");
 var ctx = c.getContext("2d");
 ctx.beginPath();
-
-for (var x = 0; x<=333; x+=1){
-   y=x*10;
-   ctx.moveTo(100-y,100+y);
-   ctx.lineTo(400+y,400-y);
-ctx.lineWidth=5;
+var y = 20;
+//ctx.moveTo(10,10);
+for (var x = 10; x<=301; x+=10){
+   ctx.moveTo(10,x);
+   ctx.lineTo(90,x); 
+   ctx.moveTo(100,x);
+   ctx.lineTo(190,x);
+   ctx.moveTo(200,x);
+   ctx.lineTo(290,x);
+   ctx.lineWidth=5;
 }
 ctx.stroke();

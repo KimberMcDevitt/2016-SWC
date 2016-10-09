9032042716e7a968055c5dcdaed6173e9888fcd6
KimberMcDevitt <ki.mcdevitt@northeastern.edu>
2016-10-09 13:33:16 -0400

    Update ps-04-333Lines.js

--- ps-04/ps-04-333Lines.js
+++ ps-04/ps-04-333Lines.js
@@ -3,27 +3,31 @@ var ctx = c.getContext("2d");
 ctx.beginPath();
 var y = 20;
 //ctx.moveTo(10,10);
-for (var x = 10; x<=330; x+=10){
+for (var x = 10; x<300; x+=10){
    ctx.moveTo(10,x);
    ctx.lineTo(30,x+10); 
-   ctx.moveTo(40,x+10);
+   ctx.moveTo(30,x+10);
    ctx.lineTo(60,x);
-   ctx.moveTo(70,x);
+   ctx.moveTo(60,x);
    ctx.lineTo(90,x+10);
-   ctx.moveTo(100,x+10);
+   ctx.moveTo(90,x+10);
    ctx.lineTo(120,x);
-   ctx.moveTo(130,x);
+   ctx.moveTo(120,x);
    ctx.lineTo(150,x+10);
-   ctx.moveTo(160,x+10);
+   ctx.moveTo(150,x+10);
    ctx.lineTo(180,x);
-   ctx.moveTo(190,x);
+   ctx.moveTo(180,x);
    ctx.lineTo(210,x+10);
-   ctx.moveTo(220,x+10);
+   ctx.moveTo(210,x+10);
    ctx.lineTo(240,x);
-   ctx.moveTo(250,x);
+   ctx.moveTo(240,x);
    ctx.lineTo(270,x+10);
-   ctx.moveTo(280,x+10);
+   ctx.moveTo(270,x+10);
    ctx.lineTo(300,x);
+   if (x % 2 == 0)
+     //ctx.moveTo(10,10);
+     ctx.lineTo(300,300);
+   else;
    ctx.lineWidth=5;
 }
 ctx.stroke();

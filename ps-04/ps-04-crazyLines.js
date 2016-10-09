e48978546adc68bfb83e967c89e37ee0006060b1
KimberMcDevitt <ki.mcdevitt@northeastern.edu>
2016-10-08 21:08:01 -0400

    Update ps-04-crazyLines.js

--- ps-04/ps-04-crazyLines.js
+++ ps-04/ps-04-crazyLines.js
@@ -1,7 +1,7 @@
 var c = document.getElementById("canvasElm");
 var ctx = c.getContext("2d");
 ctx.beginPath();
-for (var x = 0; x<=330; x+=10){
+for (var x = 0; x<330; x+=10){
 ctx.moveTo(20+x,20);
 ctx.lineTo(20+x,100+x);
 ctx.lineTo(100+x,100+x);

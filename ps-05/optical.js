5792373ae94bc5e7125f507c6e62af47119feb44
KimberMcDevitt <ki.mcdevitt@northeastern.edu>
2016-10-15 22:52:35 -0400

    Update optical.js

--- ps-05/optical.js
+++ ps-05/optical.js
@@ -1,19 +1,45 @@
-var myCanvas = document.getElementById('canvasOI');
+var canvas = document.getElementById('canvasOI');
+var context = canvas.getContext('2d');
+var centerX = canvas.width / 4;
+var centerY = canvas.height / 4;
+var radius = 70;
 
-var ctx = myCanvas.getContext("2d");
-var rotation = 0;
+      context.beginPath();
+      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = '#3F0367';
+      context.fill();
+      context.lineWidth = 5;
+      context.strokeStyle = '#3F0367';
+      context.stroke();
 
-ctx.lineWidth=10;
-ctx.beginPath();
+      context.beginPath();
+      context.arc(centerX*2, centerY*3, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = '#3F0367';
+      context.fill();
+      context.lineWidth = 5;
+      context.strokeStyle = '#3F0367';
+      context.stroke();
 
-var drawItem = function(drop, rotation) {
-  //ctx.rotate(rotation * degToRad);
-  ctx.beginPath();
-  ctx.moveTo(drop,20);
-  ctx.bezierCurveTo(drop,100,200,100,200,drop);
-  ctx.stroke();}
+      context.beginPath();
+      context.arc(centerX*3, centerY, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = '#3F0367';
+      context.fill();
+      context.lineWidth = 5;
+      context.strokeStyle = '#3F0367';
+      context.stroke();
 
-for (x=0; x<10; x++){
-  drawItem(x*50)
-}
-
+// the triangle
+context.beginPath();
+context.moveTo(centerX, centerY);
+context.lineTo(centerX*3, centerY);
+context.lineTo(centerX*2, centerY*3);
+context.closePath();
+ 
+// the outline
+context.lineWidth = 10;
+context.strokeStyle = '#DECDFD';
+context.stroke();
+ 
+// the fill color
+context.fillStyle = "#DECDFD";
+context.fill();

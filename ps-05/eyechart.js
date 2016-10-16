21747c1af81dc0a25b1caccd65ef526c3bab4730
KimberMcDevitt <ki.mcdevitt@northeastern.edu>
2016-10-15 21:24:47 -0400

    Update eyechart.js

--- ps-05/eyechart.js
+++ ps-05/eyechart.js
@@ -23,7 +23,7 @@ var drawE1 = function (Col, Row, lineLength) {
 var drawE2 = function (Col, Row, lineLength) {
 
   cx.strokeStyle="#017464";
-  cx.lineWidth=25;
+  cx.lineWidth=15;
   cx.moveTo (Col,Row);
   cx.lineTo(Col+lineLength, Row);
   cx.lineTo(Col+lineLength, Row+lineLength);
@@ -38,7 +38,7 @@ var drawE2 = function (Col, Row, lineLength) {
 var drawE3 = function (Col, Row, lineLength) {
 
   cx.strokeStyle="#F7820C";
-  cx.lineWidth=25;
+  cx.lineWidth=15;
   cx.moveTo (Col,Row);
   cx.lineTo(Col, Row+lineLength);
   cx.lineTo(Col+lineLength, Row+lineLength);
@@ -58,10 +58,10 @@ for (var C = 1; C<6; C+=1){
         var padding = size;
         var testCol = C % 2 ;
         var int = parseInt((Math.random() * (7 - 1 + 1)), 10) + 1;
-        if (int == 1){
+        if (int == 1 || int==3 ){
           drawE1(colAdj, rowAdj+size, size);
         } else {
-          if(int==2){
+          if(int == 2 || int == 4){
              drawE2(colAdj,rowAdj+size,size);
         } else {
              drawE3(colAdj,rowAdj+size,size);

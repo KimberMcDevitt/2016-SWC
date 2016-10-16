6fcffd90e4ba713b6c6b33cae2112aafad45ae90
KimberMcDevitt <ki.mcdevitt@northeastern.edu>
2016-10-15 21:12:25 -0400

    Update eyechart.js

--- ps-05/eyechart.js
+++ ps-05/eyechart.js
@@ -1,11 +1,71 @@
 var myCanvas = document.getElementById('canvasEye');
 var drawingPad = myCanvas.getContext('2d');
 
-var numLines = 33;
 var cx = document.querySelector('canvas').getContext('2d');
 cx.beginPath();
-for (var y = 10; y<10*(numLines+2); y+=10){
-  cx.moveTo(10+y,y);
-  cx.lineTo(200,y);
+var rotation = 0;
+
+var drawE1 = function (Col, Row, lineLength) {
+
+  cx.strokeStyle="#FF0000";
+  cx.lineWidth=25;
+  cx.moveTo (Col,Row);
+  cx.lineTo(Col+lineLength, Row);
+  cx.moveTo(Col,Row);
+  cx.lineTo (Col,Row+lineLength);
+  cx.lineTo(Col+lineLength, Row+lineLength);
+  cx.moveTo(Col,Row+lineLength/2)
+  cx.lineTo(Col+lineLength,Row+lineLength/2);
+  cx.stroke ();
+
 }
-cx.stroke();
+
+var drawE2 = function (Col, Row, lineLength) {
+
+  cx.strokeStyle="#017464";
+  cx.lineWidth=25;
+  cx.moveTo (Col,Row);
+  cx.lineTo(Col+lineLength, Row);
+  cx.lineTo(Col+lineLength, Row+lineLength);
+  cx.moveTo(Col,Row+lineLength/2)
+  cx.lineTo(Col+lineLength,Row+lineLength/2);
+  cx.moveTo(Col+lineLength,Row+lineLength);
+  cx.lineTo (Col,Row+lineLength);
+  cx.stroke ();
+
+}
+
+var drawE3 = function (Col, Row, lineLength) {
+
+  cx.strokeStyle="#F7820C";
+  cx.lineWidth=25;
+  cx.moveTo (Col,Row);
+  cx.lineTo(Col, Row+lineLength);
+  cx.lineTo(Col+lineLength, Row+lineLength);
+  cx.lineTo(Col+lineLength,Row);
+  cx.moveTo(Col+lineLength/2,Row+lineLength);
+  cx.lineTo (Col+lineLength/2,Row);
+  cx.stroke ();
+
+}
+
+cx.beginPath();   
+for (var C = 1; C<6; C+=1){
+      for(var R = 1; R<5; R++){
+        var size = 100-(20*R);
+        var colAdj = C*2*size;
+        var rowAdj = R*150;
+        var padding = size;
+        var testCol = C % 2 ;
+        var int = parseInt((Math.random() * (7 - 1 + 1)), 10) + 1;
+        if (int == 1){
+          drawE1(colAdj, rowAdj+size, size);
+        } else {
+          if(int==2){
+             drawE2(colAdj,rowAdj+size,size);
+        } else {
+             drawE3(colAdj,rowAdj+size,size);
+        }}
+        
+      }
+}

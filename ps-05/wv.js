b34d8d72f3289dca8727996a77da9511f50333ea
KimberMcDevitt <ki.mcdevitt@northeastern.edu>
2016-10-17 17:57:03 -0400

    Create wv.js

---
+++ ps-05/wv.js
@@ -0,0 +1,19 @@
+var myCanvas = document.getElementById('canvasOI');
+
+var ctx = myCanvas.getContext("2d");
+var rotation = 0;
+
+ctx.lineWidth=10;
+ctx.beginPath();
+
+var drawItem = function(drop, rotation) {
+  //ctx.rotate(rotation * degToRad);
+  ctx.beginPath();
+  ctx.moveTo(drop,20);
+  ctx.bezierCurveTo(drop,100,200,100,200,drop);
+  ctx.stroke();}
+
+for (x=0; x<13; x++){
+  drawItem(x*50)
+}
+

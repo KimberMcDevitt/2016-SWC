4d96990694191986720d70626fa11e01cee2a6f5
KimberMcDevitt <ki.mcdevitt@northeastern.edu>
2016-12-10 15:54:58 -0500

    Update nodes.js

--- ps-11/nodes.js
+++ ps-11/nodes.js
@@ -1,7 +1,7 @@
 // code borrowed from Ross Kirsling http://bl.ocks.org/rkirsling/5001347
 // set up SVG for D3
 var width = 2000,
-  height = 400,
+  height = 600,
   colors = d3.scale.category10();
 
 var svg = d3.select('body')
